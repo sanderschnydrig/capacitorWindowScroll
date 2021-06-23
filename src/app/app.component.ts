602293ef770d9f297f2f96b212d16e5d1881c5f1
Sander <s.schnydrig@mengisgruppe.ch>
2021-06-23 15:11:07 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 import { Plugins } from '@capacitor/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'capacitorWindowScroll';
 
   @HostListener('window:scroll', ['$event'])
   onScroll(event) {
     console.log('scrolling: ', event)
+  }
+
+  ngAfterViewInit() {
+    document.addEventListener('scroll', function(e) {
+      console.log('document:scroll: ', e)
+    })
+
+    const scroll$ = fromEvent(window, 'scroll')
+    scroll$.subscribe((ev) => {
+      console.log('fromEvent scroll: ', ev)
+    })
   }
 }
